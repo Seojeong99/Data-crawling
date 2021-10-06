cc2a698b3a59cfdedf357662b2864a4549174be6
Seojeong99 <pinkberry99@naver.com>
2021-10-06 20:50:50 +0900

    21.10.06

---
+++ crawlingTest2.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from bs4 import BeautifulSoup
+from pprint import pprint
+import requests
+from urllib.request import urlretrieve
+
+
+driver = webdriver.Firefox(executable_path="C:/driver/geckodriver.exe")
+driver.wait = WebDriverWait(driver, 2)
+
+URL1 = "http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=E04096&cctvname=%25EC%259B%2594%25EA%25B3%25841%25EA%25B5%2590-%25EB%2585%25B9%25EC%25B2%259C%25EA%25B5%2590&kind=A&cctvip=176&cctvch=null&id=null&cctvpasswd=null&cctvport=932&minX=127.03624212566622&minY=37.6356911150544&maxX=127.08718166625532&maxY=37.65900425441203"
+driver.get(URL1)
+time.sleep(3)
+
+images = driver.find_element_by_xpath('/html/body/div/div/object/div').screenshot("C:/please/a.png")
