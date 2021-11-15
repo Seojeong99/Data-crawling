260b8a101cd2f2863ca4f293571910a50fc0329e
Seojeong99 <pinkberry99@naver.com>
2021-11-16 04:15:39 +0900

    21.11.16

--- motorcycle.py
+++ motorcycle.py
@@ -11,6 +11,7 @@ paths = glob.glob('D:/traingData/rm_0/*.jpg')
 paths = np.random.permutation(paths)
 독립 = np.array([plt.imread(paths[i]) for i in range(len(paths))])
 종속 = np.array([paths[i].split('/')[-2] for i in range(len(paths))])
+
 print(독립.shape, 종속.shape)
 '''
 독립 = 독립.reshape(233, 400, 850, 1)
