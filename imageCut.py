50492cc896b3139449b23c0529df4651abd66d86
Seojeong99 <pinkberry99@naver.com>
2021-11-24 17:06:26 +0900

    21.11.24

--- imageCut.py
+++ imageCut.py
@@ -45,10 +45,10 @@ class TimerThread(threading.Thread):
 if __name__ == '__main__':
     driver = webdriver.Firefox(executable_path="C:/driver/geckodriver.exe")
     driver.wait = WebDriverWait(driver, 3)
-    URL1="http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L010144&cctvname=%25EC%2598%25AC%25EB%25A6%25BC%25ED%2594%25BD%25EA%25B3%25B5%25EC%259B%2590&kind=Seoul&cctvip=null&cctvch=52&id=167&cctvpasswd=null&cctvport=null&minX=127.1006466052482&minY=37.50181730698737&maxX=127.13109817174727&maxY=37.527114302340955"
+    #URL1="http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L010144&cctvname=%25EC%2598%25AC%25EB%25A6%25BC%25ED%2594%25BD%25EA%25B3%25B5%25EC%259B%2590&kind=Seoul&cctvip=null&cctvch=52&id=167&cctvpasswd=null&cctvport=null&minX=127.1006466052482&minY=37.50181730698737&maxX=127.13109817174727&maxY=37.527114302340955"
     #URL1 = "http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L010214&cctvname=%25EC%2598%25AC%25EB%25A6%25BC%25ED%2594%25BD%25EB%258C%2580%25EA%25B5%2590%25EB%2582%25A8%25EB%258B%25A8&kind=Seoul&cctvip=null&cctvch=52&id=270&cctvpasswd=null&cctvport=null&minX=127.09912824468147&minY=37.50792746385415&maxX=127.15004245416765&maxY=37.54851834572314"
     #URL1 = "http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L010193&cctvname=%25EC%25B0%25BD%25EB%258D%2595%25EA%25B6%2581R&kind=Seoul&cctvip=null&cctvch=52&id=249&cctvpasswd=null&cctvport=null&minX=126.97656150723212&minY=37.560806567436394&maxX=127.007003856218&maxY=37.58612490336802"
-    #URL1 = "http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L010098&cctvname=%25EC%25A2%2585%25EB%25A1%259C1%25EA%25B0%2580&kind=Seoul&cctvip=null&cctvch=52&id=121&cctvpasswd=null&cctvport=null&minX=126.97260204133853&minY=37.55362493982439&maxX=126.99498168111192&maxY=37.57894357098292"
+    URL1 = "http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L010098&cctvname=%25EC%25A2%2585%25EB%25A1%259C1%25EA%25B0%2580&kind=Seoul&cctvip=null&cctvch=52&id=121&cctvpasswd=null&cctvport=null&minX=126.97260204133853&minY=37.55362493982439&maxX=126.99498168111192&maxY=37.57894357098292"
     driver.get(URL1)
     time.sleep(5)
     # driver.find_element_by_xpath('/html/body/div/div/object/div/div/div[4]/button[2]/span[1]').click()
@@ -66,11 +66,12 @@ if __name__ == '__main__':
                                        + get_today() + '-' + time.strftime('%H')
                                        + '-' + time.strftime('%M')
                                        + '-' + time.strftime('%S') + '.jpg',
-                                       region=(620, 571, 300, 350))
+                                       #region=(620, 571, 300, 350))
                                        #region=(873, 536, 250, 380))#앞이 폭 뒤가 높이
                                        #region=(242, 683, 850, 400))
-                                       #region=(460,644,400,230))
-            # region=(816, 199, 420, 280))
-            # region=(241, 115, 420, 280))
+                                       #region=(620,659,370,230))
+                                       region=(529, 716, 370, 230))
+             #region=(816, 199, 420, 280))
+             #region=(241, 115, 420, 280))
 
             time.sleep(1)
