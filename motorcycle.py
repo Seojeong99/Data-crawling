e67318d6efabca8538a693b36e490d5e51b2f8f6
Seojeong99 <pinkberry99@naver.com>
2021-12-02 17:33:03 +0900

    21.12.02

--- motorcycle.py
+++ motorcycle.py
@@ -12,7 +12,7 @@ paths = np.random.permutation(paths)
 독립 = np.array([plt.imread(paths[i]) for i in range(len(paths))])
 종속 = np.array([paths[i].split('/')[-2] for i in range(len(paths))])
 
-print(독립.shape, 종속.shape)
+print(독립 .shape, 종속.shape)
 '''
 독립 = 독립.reshape(233, 400, 850, 1)
 종속 = pd.get_dummies(종속)
