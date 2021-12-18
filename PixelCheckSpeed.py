4e534af2bff58ae45dd08c1e0fe1023dc401fd42
Seojeong99 <pinkberry99@naver.com>
2021-12-18 20:13:39 +0900

    21.12.18

--- PixelCheckSpeed.py
+++ PixelCheckSpeed.py
@@ -19,6 +19,7 @@ def popcnt():
     time.sleep(1)
     tmp = driver.window_handles
     print(tmp)
+
     #driver.switch_to.window(driver.window_handles[1])
     #driver.close()
     #driver.switch_to.window(driver.window_handles[0])
