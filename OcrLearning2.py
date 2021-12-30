016825d5fcd8f1b439f1be74f82b36ad9bea5b15
Seojeong99 <pinkberry99@naver.com>
2021-12-30 23:33:00 +0900

    21.12.30

--- OcrLearning2.py
+++ OcrLearning2.py
@@ -4,4 +4,6 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd= R'C:\Program Files\Tesseract-OCR\tesseract'
 str = pytesseract.image_to_string(Image.open('D:\OcrTest\Music.png'), lang='kor')
 
+
+
 print(str)
